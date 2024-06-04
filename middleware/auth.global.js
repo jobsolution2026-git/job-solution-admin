e30595e7941d7a62de2712253550f142a3c682f8
mosabberul55 <mosabberulislam55@gmail.com>
2024-06-04 19:48:46 +0600

    global middleware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,8 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore()
   const {isLoggedIn} = storeToRefs(authStore)
-  console.log('isLoggedIn', isLoggedIn.value)
-  console.log('layout', to)
   if (!isLoggedIn.value && to.meta.layout !== 'blank') {
     return navigateTo('/login')
   }
