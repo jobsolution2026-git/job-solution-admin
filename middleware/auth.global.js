75f07dc52749e332c341863a2ae903793bd1f5c7
mosabberul55 <mosabberulislam55@gmail.com>
2024-06-04 19:20:50 +0600

    fix middleware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,9 +2,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore()
   const { isLoggedIn } = storeToRefs(authStore)
 
-  // Check if user is not logged in and not already on the login page
   if (!isLoggedIn.value && to.path !== '/login' && to.meta.layout !== 'blank') {
-    // Redirect to login page with the next query parameter
     return navigateTo('/login?next=' + to.path)
   }
 });
--- middleware/guest.js
+++ middleware/guest.js
@@ -2,9 +2,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore()
   const { isLoggedIn } = storeToRefs(authStore)
 
-  // Check if user is logged in and trying to access guest-only pages (e.g., login or signup)
   if (isLoggedIn.value && (to.path === '/login')) {
-    // Redirect to the home page
     return navigateTo('/')
   }
 });
