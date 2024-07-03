102f9b15ba4ae1774588f4378892944669f0ae65
mosabberul55 <mosabberulislam55@gmail.com>
2024-07-03 17:21:47 +0600

    exam

--- pages/questions/[id]/exam.vue
+++ pages/questions/[id]/exam.vue
@@ -212,7 +212,7 @@ const selectAndUnselectAll = () => {
                 <div v-if="mcq?.tags" class="flex gap-2 flex-wrap mb-2">
                   <span>Tags: </span>
                   <div v-for="(tag,i) in mcq.tags" :key="i">
-                    <small class="bg-yellow-400 rounded p-1">{{tag}}</small>
+                    <small class="bg-yellow-600 rounded p-1 text-white">{{ tag }}</small>
                   </div>
                 </div>
                 <div v-if="mcq.explanation" class="text-sm font-medium text-gray-900 dark:text-white max-w-xl text-wrap">Explanation: <span v-katex="mcq.explanation" class="latex"></span></div>
