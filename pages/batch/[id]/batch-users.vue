b1d89b15ae99a6aeb148e6cb903be3f62b705093
User <beparypartho@gmail.com>
2024-08-19 21:39:55 +0600

    batch user

---
+++ pages/batch/[id]/batch-users.vue
@@ -0,0 +1,147 @@
+<script setup lang="ts">
+import type {PageInfo} from "~/interfaces/pageinfo";
+import type {Loader} from "~/interfaces/loader";
+import {useForm} from "vee-validate";
+import * as yup from "yup";
+import {useBatchStore} from "~/stores/batch";
+
+interface User {
+  id: number
+  name: string
+}
+
+const users = ref<User>();
+const route = useRoute();
+
+const query = ref<string>('');
+const filteredUsers = ref([])
+const selectedUser = ref<User>()
+const batch_id = route.params.id
+
+const pageInfo = ref<PageInfo>({
+  title: 'Users',
+  description: 'Manage all your Users here',
+  apiUrl: '/admin/batch-users',
+});
+
+useHead({title: `Manage ${pageInfo.value.title}`});
+const loader = ref<Loader>({
+  isLoading: false,
+  isSubmitting: false,
+});
+
+const batchStore = useBatchStore();
+
+if (batchStore.batches && batchStore.batches.length < 1) {
+  batchStore.fetchBatches();
+}
+
+
+//form
+const {errors, handleSubmit, handleReset, defineField, setErrors} = useForm({
+  validationSchema: yup.object({
+    validity: yup.string().min(0).required(),
+    amount: yup.number().min(0).nullable(),
+  }),
+  initialValues: {
+    validity: '',
+    amount: null,
+  },
+});
+//form fields
+const [validity, validityAttrs] = defineField('validity');
+const [amount, amountAttrs] = defineField('amount');
+
+
+const onSubmit = handleSubmit(async values => {
+  let url = pageInfo.value.apiUrl;
+  values['user_id'] = selectedUser?.value?.id;
+  values['batch_id'] = batch_id;
+  const {data, error} = await postData(url, values);
+  if (error && error.value) {
+    showToast('error', 'An error occurred while deleting the item');
+  } else {
+    handleReset();
+    showToast('success', 'User added successfully');
+  }
+});
+
+const handleSearch = async() => {
+  if (query.value.length > 2) {
+    const {data, error} = await getData('/admin/users?search=' + query.value);
+    if (error && error.value) {
+      showToast('error', 'An error occurred while searching for users');
+    } else {
+      filteredUsers.value = data?.value as User[]
+    }
+  }
+}
+
+const handleSelectUser = (user) => {
+  selectedUser.value = user
+  query.value = user.name
+  filteredUsers.value = []
+}
+
+const getBatchUsers = async() => {
+  const {data, error} = await getData(`/admin/batch-user?batch_id=${batch_id}`);
+  if (error && error.value) {
+    showToast('error', 'An error occurred while fetching batch users');
+  } else {
+    users.value = data?.value as User[]
+  }
+}
+
+await getBatchUsers()
+
+</script>
+
+<template>
+  <section class="py-3 sm:py-5">
+    <div class="px-4 mx-auto max-w-xl lg:px-12 ng-white dark:bg-gray-800 py-12 rounded border-2 dark:border-gray-700">
+      <form @submit.prevent="onSubmit">
+        <div class="grid gap-4 mb-4 sm:grid-cols-2">
+          <div class="col-span-2">
+            <form-input-label label="Search for a user"/>
+            <div class="relative w-full">
+              <input
+                  required
+                  type="text"
+                  v-model="query"
+                  @input="handleSearch"
+                  class="w-full rounded focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search for a user..."
+              />
+              <ul v-if="filteredUsers.length > 0" class="absolute left-0 w-full mt-2 bg-gray-200 border sm:text-sm rounded-lg block w-full p-2.5">
+                <li
+                    v-for="user in filteredUsers"
+                    :key="user.id"
+                    @click="handleSelectUser(user)"
+                    class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                >
+                  {{ user.name }}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-span-2">
+            <form-input-label label="Validity"/>
+            <form-date-time-picker type="date" v-model="validity" v-bind="validityAttrs"
+                                   :error="errors.validity"/>
+            <form-input-error :message="errors.validity"/>
+          </div>
+        </div>
+        <div class="flex justify-end gap-2">
+          <button type="submit"
+                  class="px-8 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
--- pages/batch/index.vue
+++ pages/batch/index.vue
@@ -187,8 +187,8 @@ const submitSuccess = (item: object, msg: string) => {
                 </td>
                 <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   <div class="flex items-center space-x-2">
-                    <button @click="editItem(item)"
-                             class="px-3 py-2 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
+                    <nuxt-link :to="`/batch/${item.id}/batch-users`" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Users</nuxt-link>
+                    <button @click="editItem(item)" class="px-3 py-2 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
                     <common-delete-modal :id="item.id" @update="deleteItem($event)"/>
                   </div>
                 </td>
